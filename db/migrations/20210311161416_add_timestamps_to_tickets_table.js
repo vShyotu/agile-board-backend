df196ba9a9907babc04fb1194c1ac519c5eb0d32
Grant Hardman <grant.r.hardman@outlook.com>
2021-03-11 20:28:43 +0000

    Refactor timestamps migration to include update_timestamp trigger

--- db/migrations/20210311161416_add_timestamps_to_tickets_table.js
+++ db/migrations/20210311161416_add_timestamps_to_tickets_table.js
@@ -1,11 +1,35 @@
-exports.up = function (knex) {
-  return knex.schema.table("tickets", (table) => {
-    table.timestamps(true, true);
+exports.up = async function (knex) {
+  await knex.schema.table("tickets", (table) => {
+    table.timestamps(false, true);
   });
+
+  await knex.raw(`
+    CREATE OR REPLACE FUNCTION update_timestamp() RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS
+    $$
+    BEGIN
+      NEW.updated_at = CURRENT_TIMESTAMP;
+      RETURN NEW;
+    END;
+    $$;
+  `);
+
+  await knex.raw(`
+    CREATE TRIGGER update_timestamp
+    BEFORE UPDATE
+    ON tickets
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_timestamp()
+  `);
 };
 
-exports.down = function (knex) {
-  return knex.schema.table("tickets", (table) => {
+exports.down = async function (knex) {
+  await knex.schema.table("tickets", (table) => {
     table.dropTimestamps();
   });
+
+  await knex.raw(`
+    DROP FUNCTION IF EXISTS update_timestamp() CASCADE;
+  `);
 };
